f61eb4b986352f30833e0c544f0fa9c56575934b
Arda Özkardeşler <aozkardesler@gmail.com>
2023-10-13 15:10:24 +0300

    Read grip button value & animate fist accordingly

--- Assets/Scripts/VR/HandAnimation.cs
+++ Assets/Scripts/VR/HandAnimation.cs
@@ -6,11 +6,15 @@ using UnityEngine.InputSystem;
 public class HandAnimation : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
 
     void Update()
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
     }
 }
