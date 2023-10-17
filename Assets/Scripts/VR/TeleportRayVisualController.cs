8a276f2a725afbd820f6fb5578579f1bed5e2e87
Arda Özkardeşler <aozkardesler@gmail.com>
2023-10-17 18:08:05 +0300

    Make the interactor line visual visible only when the A button is held down

---
+++ Assets/Scripts/VR/TeleportRayVisualController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportRayVisualController : MonoBehaviour
+{
+    public InputActionProperty primaryButtonAction;
+    public GameObject teleportRay;
+    public float activationThreshold = 0.1f;
+
+    void Update()
+    {
+        teleportRay.SetActive(CheckIfActivated());
+    }
+
+    public bool CheckIfActivated()
+    {
+        if (primaryButtonAction.action.ReadValue<float>() > activationThreshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
