6efd30698bb060b0d300fdc59b972d04e119c491
Arda Özkardeşler <aozkardesler@gmail.com>
2023-10-25 12:25:29 +0300

    Add hand menu settings script

---
+++ Assets/Scripts/VR/Settings Scripts/HandMenuSettings.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandMenuSettings : MonoBehaviour
+{
+    GameAPI gameAPI;
+
+    public Toggle continuousMovementToggle;
+    public Toggle teleportationMovementToggle;
+    public Toggle continuousRotationToggle;
+    public Toggle snapRotationToggle;
+    public Toggle tunnelingVignetteOnToggle;
+    public Toggle tunnelingVignetteOffToggle;
+    private void Awake()
+    {
+        gameAPI = Camera.main.GetComponent<GameAPI>();
+    }
+
+    void Start()
+    {
+        GetPreferences();
+    }
+
+    private void GetPreferences()
+    {
+        continuousMovementToggle.isOn = gameAPI.GetMovementTypePreference() == "Continuous" ? true : false;
+        teleportationMovementToggle.isOn = gameAPI.GetMovementTypePreference() == "Teleportation" ? true : false;
+        continuousRotationToggle.isOn = gameAPI.GetRotationTypePreference() == "Continuous" ? true : false;
+        snapRotationToggle.isOn = gameAPI.GetRotationTypePreference() == "Snap" ? true : false;
+        tunnelingVignetteOnToggle.isOn = gameAPI.GetTunnelingVignettePreference() == 1 ? true : false;
+        tunnelingVignetteOffToggle.isOn = gameAPI.GetTunnelingVignettePreference() == 0 ? true : false;
+    }
+
+}
