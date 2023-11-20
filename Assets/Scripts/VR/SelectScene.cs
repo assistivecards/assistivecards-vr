8a6ac8570ca775e360d2c028b9489ee602737a3f
Arda Özkardeşler <aozkardesler@gmail.com>
2023-11-20 16:44:16 +0300

    Adjust layout groups & detect selected scene

---
+++ Assets/Scripts/VR/SelectScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectScene : MonoBehaviour
+{
+    public void DetectSelectedScene()
+    {
+        Debug.Log(gameObject.name);
+    }
+}
