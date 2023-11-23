8b308eb4b01717b43af94238deeceb58ca8bf25c
Arda Özkardeşler <aozkardesler@gmail.com>
2023-11-23 15:56:16 +0300

    Play poke animation on lamp button press

--- Assets/Scripts/VR/Lamp.cs
+++ Assets/Scripts/VR/Lamp.cs
@@ -5,10 +5,47 @@ using UnityEngine;
 public class Lamp : MonoBehaviour
 {
     [SerializeField] GameObject lampLight;
+    bool isPressing;
+    Transform pressingHand;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Poke")
+        {
+            pressingHand = other.name.Contains("r_index") ? GameObject.Find("Right Hand Model").transform : GameObject.Find("Left Hand Model").transform;
+            EnablePokeAnim();
             lampLight.SetActive(!lampLight.activeInHierarchy);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        DisablePokeAnim();
+    }
+
+    private void Update()
+    {
+        if (pressingHand != null)
+        {
+            if (isPressing)
+            {
+                pressingHand.GetComponent<Animator>().SetFloat("Poke", 1, .1f, Time.deltaTime);
+            }
+            else if (!isPressing)
+            {
+                pressingHand.GetComponent<Animator>().SetFloat("Poke", 0, .1f, Time.deltaTime);
+            }
+        }
+
+    }
+
+    public void EnablePokeAnim()
+    {
+        isPressing = true;
+    }
+
+    public void DisablePokeAnim()
+    {
+        isPressing = false;
     }
 }
